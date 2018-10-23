51f98868ac67f85703497a57b2eac71d115d4a68
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2018-10-23 19:40:39 +0200

    /Users/anatoly/.antigen/bundles/eendroroy/alien full dir in promt

--- alien.zsh
+++ alien.zsh
@@ -11,8 +11,8 @@ source "${THEME_ROOT}/libs/zsh-256color/zsh-256color.plugin.zsh"
 source "${THEME_ROOT}/modules/theme.zsh"
 source "${THEME_ROOT}/modules/system.zsh"
 source "${THEME_ROOT}/modules/git.zsh"
-source "${THEME_ROOT}/modules/hg.zsh"
-source "${THEME_ROOT}/modules/svn.zsh"
+#source "${THEME_ROOT}/modules/hg.zsh"
+#source "${THEME_ROOT}/modules/svn.zsh"
 source "${THEME_ROOT}/modules/async.zsh"
 source "${THEME_ROOT}/modules/python.zsh"
 
@@ -29,7 +29,7 @@ function precmd(){
   fi
   
   PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %~ %f%k%F{$color5}%f
+%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f
 %F{$color3}$(alien_ssh_client)%f%F{$color14}`alien_venv`%f%F{$color8}%B❱%b%f '
   alien_async_prompt
 }
