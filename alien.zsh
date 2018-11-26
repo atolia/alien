a26f4bf7792067e0046cdaf117acf45dd7143007
Anatoly <anatoly@ip-10-68-5-62.eu-central-1.compute.internal>
2018-11-26 16:01:17 +0100

    kube-context-promt

--- alien.zsh
+++ alien.zsh
@@ -11,10 +11,10 @@ source "${THEME_ROOT}/libs/zsh-256color/zsh-256color.plugin.zsh"
 source "${THEME_ROOT}/modules/theme.zsh"
 source "${THEME_ROOT}/modules/system.zsh"
 source "${THEME_ROOT}/modules/git.zsh"
-#source "${THEME_ROOT}/modules/hg.zsh"
+source "${THEME_ROOT}/modules/hg.zsh"
 #source "${THEME_ROOT}/modules/svn.zsh"
 source "${THEME_ROOT}/modules/async.zsh"
-source "${THEME_ROOT}/modules/python.zsh"
+#source "${THEME_ROOT}/modules/python.zsh"
 
 function precmd(){
   autoload -U add-zsh-hook
@@ -27,9 +27,10 @@ function precmd(){
   else
     _user=`whoami`
   fi
-  
+
   PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f
-%F{$color3}$(alien_ssh_client)%f%F{$color14}`alien_venv`%f%F{$color8}%B❱%b%f '
+%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) $(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
   alien_async_prompt
 }
+
+#kube_ps1 alien_venv
