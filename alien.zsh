4cae1ada060e36013bbab9079a2511d80d1b5131
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2018-12-03 13:38:55 +0100

    twolines-promt-compatible-mc

--- alien.zsh
+++ alien.zsh
@@ -11,7 +11,7 @@ source "${THEME_ROOT}/libs/zsh-256color/zsh-256color.plugin.zsh"
 source "${THEME_ROOT}/modules/theme.zsh"
 source "${THEME_ROOT}/modules/system.zsh"
 source "${THEME_ROOT}/modules/git.zsh"
-source "${THEME_ROOT}/modules/hg.zsh"
+#source "${THEME_ROOT}/modules/hg.zsh"
 #source "${THEME_ROOT}/modules/svn.zsh"
 source "${THEME_ROOT}/modules/async.zsh"
 #source "${THEME_ROOT}/modules/python.zsh"
@@ -28,8 +28,11 @@ function precmd(){
     _user=`whoami`
   fi
 
-  PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) $(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
+#  PROMPT='
+#%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) $(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
+  PROMPT='$(alien_date_time_info) %d
+$(kube_ps1):'
+
   alien_async_prompt
 }
 
--- modules/async.zsh
+++ modules/async.zsh
@@ -3,12 +3,16 @@
 function alien_dummy(){}
 
 function alien_lprompt_complete() {
+
   if [[ $(alien_is_git) == 1 ]]; then
-    PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info) $(alien_battery_stat)$(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%K{$color7}%k%f%K{$color7}%F{$color9}`alien_git_branch`%f%k%K{$color10}%F{$color7}%f%k%K{$color10}%F{$color11}$(alien_git_lr) %f%k%K{$color12}%F{$color10}%f%k%K{$color12}%F{$color13}$(alien_git_dirty) %f%k%F{$color12}%f '
-  else
-    PROMPT='
-%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info) $(alien_battery_stat)$(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
+#    PROMPT='
+#%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info) $(alien_battery_stat)$(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%K{$color7}%k%f%K{$color7}%F{$color9}`alien_git_branch`%f%k%K{$color10}%F{$color7}%f%k%K{$color10}%F{$color11}$(alien_git_lr) %f%k%K{$color12}%F{$color10}%f%k%K{$color12}%F{$color13}$(alien_git_dirty) %f%k%F{$color12}%f '
+    PROMPT=$PROMPT'`alien_git_branch`%f%k%K{$color12}%F{$color13}$(alien_git_dirty)%F{$color6}$(alien_git_lr) %f%k%F{$color12} '
+#  else
+#    PROMPT='
+#%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info) $(alien_battery_stat)$(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
+#    PROMPT='
+#$(alien_date_time_info)$(kube_ps1)%~'
   fi
   zle && zle reset-prompt
   async_stop_worker lprompt -n
