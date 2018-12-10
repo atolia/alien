38241da698d1d65fc1287a3551e03b1e9d1bb7b0
Anatoly <anatoly@Anatolys-MacBook-Pro.local>
2018-12-10 22:57:13 +0100

    pip-venv

--- alien.zsh
+++ alien.zsh
@@ -14,7 +14,7 @@ source "${THEME_ROOT}/modules/git.zsh"
 #source "${THEME_ROOT}/modules/hg.zsh"
 #source "${THEME_ROOT}/modules/svn.zsh"
 source "${THEME_ROOT}/modules/async.zsh"
-#source "${THEME_ROOT}/modules/python.zsh"
+source "${THEME_ROOT}/modules/python.zsh"
 
 function precmd(){
   autoload -U add-zsh-hook
@@ -31,7 +31,7 @@ function precmd(){
 #  PROMPT='
 #%(?.%K{$color0}%F{$color1}%f%k.%K{$color0}%F{$color1r}%f%k)%K{$color0}%F{$color2} $(alien_date_time_info)$(alien_battery_stat) $(kube_ps1) %f%k%K{$color3}%F{$color0}%f%k%K{$color3}%F{$color4} $_user %f%k%K{$color5}%F{$color3}%f%k%K{$color5}%F{$color6} %d %f%k%F{$color5}%f '
   PROMPT='$(alien_date_time_info) %d
-$(kube_ps1):'
+$(alien_venv)$(kube_ps1):'
 
   alien_async_prompt
 }
